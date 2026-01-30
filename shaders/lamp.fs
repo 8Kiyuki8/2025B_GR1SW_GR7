ea7bf562d920968fde442956b955764a6e2a8962
“Divad-exe“ <david.guanochanga@epn.edu.ec>
2026-01-30 02:10:55 -0500

    David - actualice el lamp.fs

--- shaders/lamp.fs
+++ shaders/lamp.fs
@@ -1,7 +1,11 @@
 #version 330 core
 out vec4 FragColor;
 
+// Esta variable nos permite enviarle un color desde C++
+uniform vec3 lightColor; 
+
 void main()
 {
-    FragColor = vec4(1.0); // Color blanco brillante constante
+    // Pintamos el objeto del color que recibimos, con 1.0 de opacidad
+    FragColor = vec4(lightColor, 1.0);
 }
